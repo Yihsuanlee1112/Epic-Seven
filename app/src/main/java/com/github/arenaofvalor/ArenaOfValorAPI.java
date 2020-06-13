9cae514313435bcc6500245daa42154c7f34c084
Yihsuanlee1112 <yihsuan.rebecca.lee@gmail.com>
2020-06-13 23:12:43 +0800

    list Hero names and types

--- app/src/main/java/com/github/arenaofvalor/ArenaOfValorAPI.java
+++ app/src/main/java/com/github/arenaofvalor/ArenaOfValorAPI.java
@@ -1,1 +1,1 @@
-package com.github.arenaofvalor;import retrofit2.Call;import retrofit2.http.GET;public interface ArenaOfValorAPI {    @GET("/Yihsuanlee1112/arenaofvalor/master/app/Files/ArenaOfValor.json")    Call<ArenaOfValorResponse> getHeroResponse();}
+package com.github.arenaofvalor;import retrofit2.Call;import retrofit2.http.GET;public interface ArenaOfValorAPI {    @GET("ArenaOfValor.json")    Call<ArenaOfValorResponse> getHeroResponse();}
